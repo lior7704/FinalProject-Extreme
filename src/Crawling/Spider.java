959924f10f84ce935a1c41e2f4dfa6e24d3af430
lior7704 <37957751+lior7704@users.noreply.github.com>
2019-01-13 19:29:30 +0200

    Changed crawling method. Results for crawling with 1 parameter.

---
+++ src/Crawling/Spider.java
@@ -0,0 +1,95 @@
+package Crawling;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class Spider {
+	private static final int MAX_PAGES_TO_SEARCH = 1000;
+	private static final int MAX_PAGES_TO_GRAB = 40;
+	private Set<String> pagesVisited = new HashSet<String>();
+	private List<String> pagesToVisit = new LinkedList<String>();
+
+	/**
+	 * Our main launching point for the Spider's functionality. Internally it
+	 * creates spider legs that make an HTTP request and parse the response (the web
+	 * page).
+	 * 
+	 * @param url - The starting point of the spider
+	 * @param     searchParms[]0 - The word or string that you are searching for
+	 * @throws IOException
+	 */
+	public void search(String url, ArrayList<String> searchParams) throws IOException {
+		String currentUrl;
+		String body;
+		int numPagesGrabbed = 0;
+		while (this.pagesVisited.size() < MAX_PAGES_TO_SEARCH) {
+			SpiderLeg leg = new SpiderLeg();
+			if (this.pagesToVisit.isEmpty()) {
+				currentUrl = url;
+				this.pagesVisited.add(url);
+				leg.crawl(currentUrl);
+			} else {
+				currentUrl = this.nextUrl();
+				if (checkCrawl(currentUrl, searchParams)) {
+					leg.crawl(currentUrl);
+				} else {
+					continue;
+				}
+			}
+			// Get page if body matches parameters
+			if (currentUrl.contains("/trips")) {
+				body = leg.searchForWord(searchParams);
+				if (body != null) // parsing
+				{
+					System.out.println(String.format("**Success** Word %s found at %s", searchParams, currentUrl));
+					numPagesGrabbed++;
+					if (numPagesGrabbed == MAX_PAGES_TO_GRAB)
+						break;
+				}
+			}
+			this.pagesToVisit.addAll(leg.getLinks());
+		}
+		System.out.println("\n**Done** Visited " + this.pagesVisited.size() + " web page(s)");
+		System.out.println("Pages grabbed:" + numPagesGrabbed);
+	}
+
+	private boolean checkCrawl(String currentUrl, ArrayList<String> searchParams) {
+		if (!currentUrl.contains("#") && !currentUrl.contains(".html")) {
+			if (searchParams.size() == 1) {
+				if (currentUrl.contains(searchParams.get(0)) && !currentUrl.contains("?"))
+					return true;
+			} else if (currentUrl.contains(searchParams.get(0))
+					|| currentUrl.contains("?") && containsParams(currentUrl, searchParams)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean containsParams(String body, ArrayList<String> searchParams) {
+		for (String string : searchParams) {
+			if(!body.toLowerCase().contains(string.toLowerCase()))
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Returns the next URL to visit (in the order that they were found). We also do
+	 * a check to make sure this method doesn't return a URL that has already been
+	 * visited.
+	 * 
+	 * @return
+	 */
+	private String nextUrl() {
+		String nextUrl;
+		do {
+			nextUrl = this.pagesToVisit.remove(0);
+		} while (this.pagesVisited.contains(nextUrl));
+		this.pagesVisited.add(nextUrl);
+		return nextUrl;
+	}
+}
---
+++ src/Crawling/Test.java
@@ -0,0 +1,22 @@
+package Crawling;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Test
+{
+    /**
+     * This is our test. It creates a spider (which creates spider legs) and crawls the web.
+     * 
+     * @param args
+     *            - not used
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException
+    {
+        Spider spider = new Spider();
+        ArrayList<String> searchParams = new ArrayList<String>();
+        searchParams.add("china");
+        //searchParams.add("group");
+        spider.search("http://stridetravel.com/", searchParams);
+    }
+}
