bfd2f8abc39f068c69c9ac4ae75051aebe8a06cf
Moshe Steinvortzel <moshesss1@gmail.com>
2019-01-14 20:54:28 +0200

    Spider Changes
    Finishing searches in both sites

---
+++ src/ExtremeUtils/Utils.java
@@ -0,0 +1,9 @@
+package ExtremeUtils;
+
+public class Utils {
+	public static final String STRIDE = "STRIDE";
+	public static final String LONELY_PLANET = "LONELY_PLANET";
+	public static final String STRIDE_SITE = "https://www.stridetravel.com/";
+	public static final String LONELYPLANET_SITE = "https://www.lonelyplanet.com/";
+	public static final String LONELYPLANET_PREFIX = "/sights";
+}
