46edd8d6d3602e57740f6df404a734fdab99b7d7
lior7704 <37957751+lior7704@users.noreply.github.com>
2019-01-15 13:07:12 +0200

    almost complete lonley-planet parsing

--- src/Parsing/Converters.java
+++ src/Parsing/Converters.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Converters {
 	public static SimpleTripFormat strideConverter(String url) {
@@ -18,33 +19,17 @@ public class Converters {
 			doc = Jsoup.connect(url).get();
 			Element title = doc.select("h1").first();
 			String name = title.text();
-			Element text = doc.select("p").first();
+			Element text = doc.select("p").get(1);
 			String desc = text.text();
 			String location = null;
-			Element img = doc.select("img").get(1);
-			//String src = img.text();
-			//System.out.println("Image Found!");
-			//System.out.println("src attribute is : " + src);
-			//String image = getImage(src);
-			String image = img.attr("src");
+			String body = doc.select("body").first().data();
+			String image = (body.substring(body.indexOf("image")));
+			image = image.substring(8, image.indexOf(".jpg") + 4);
 			trip = new SimpleTripFormat(name, location, 0, 0, image, desc);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return trip;
-	}
-
-	private static String getImage(String src) {
-		//Exctract the name of the image from the src attribute
-
-		int indexname = src.lastIndexOf("/");
-		if (indexname == src.length()) {
-			src = src.substring(1, indexname);
-		}
-		indexname = src.lastIndexOf("/");
-		String name = src.substring(indexname, src.length());
-		System.out.println(name);	
-		return name;
 	}
 }
