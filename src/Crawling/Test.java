8128241328d2724a48e91360baf68c774c5c1bf0
Moshe Steinvortzel <moshesss1@gmail.com>
2019-01-16 10:40:48 +0200

    Client
    Adding client

--- src/Crawling/Test.java
+++ src/Crawling/Test.java
@@ -19,13 +19,13 @@ public class Test
     {
     	List<TripFormat> trips;
     	/// Lonely Planet Search
-        /*Spider spider = new Spider();
+        Spider spider = new Spider();
         ArrayList<String> searchParams = new ArrayList<String>();
         searchParams.add("china");
         searchParams.add("group");
         searchParams.add("panda");
         trips = spider.search(Utils.LONELY_PLANET, Utils.LONELYPLANET_SITE+searchParams.get(0)+Utils.LONELYPLANET_PREFIX, searchParams); 
-    	*/
+    	
     	/// Lonely Planet Search
 
    /*     Spider spider = new Spider();
@@ -33,10 +33,10 @@ public class Test
     	searchParams.add("france");
     	spider.search(Utils.STRIDE, Utils.STRIDE_SITE+searchParams.get(0), searchParams);*/
 
-        Spider spider = new Spider();
+    /*    Spider spider = new Spider();
     	ArrayList<String> searchParams = new ArrayList<String>();
     	searchParams.add("france");
-    	trips = spider.search(Utils.STRIDE, Utils.STRIDE_SITE+searchParams.get(0), searchParams);
+    	trips = spider.search(Utils.STRIDE, Utils.STRIDE_SITE+searchParams.get(0), searchParams);*/
     	
     	System.out.println(trips);
     }
--- src/UI/ConsoleInterface.java
+++ src/UI/ConsoleInterface.java
@@ -1,15 +1,21 @@
 package UI;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import Crawling.Spider;
 import ExtremeUtils.Utils;
+import Parsing.TripFormat;
 
 public class ConsoleInterface {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		Scanner s = new Scanner(System.in);
+		List<TripFormat> trips;
 		
 		System.out.println("**********************************");
 		System.out.println("******* Welcome to EXTREME *******");
@@ -21,11 +27,14 @@ public class ConsoleInterface {
 		
 	
 		String siteName = s.nextLine();
-		String siteUrl;
+		String siteUrl, prefix = "";
 		
-		if(siteName.toLowerCase().equals(Utils.LONELY_PLANET)) {
+		System.out.println();
+		
+		if(siteName.toLowerCase().equals(Utils.LONELY_PLANET.toLowerCase())) {
 			siteName = Utils.LONELY_PLANET;
 			siteUrl = Utils.LONELYPLANET_SITE;
+			prefix = Utils.LONELYPLANET_PREFIX;
 		}
 		else {
 			siteName = Utils.STRIDE;
@@ -35,7 +44,18 @@ public class ConsoleInterface {
 		System.out.println("Enter a state name to search in");
 		String state = s.nextLine().toLowerCase();
 		
-		 
+		System.out.println();
+		System.out.println("Please wait, performing search..");
+		
+		Spider spider = new Spider();
+    	ArrayList<String> searchParams = new ArrayList<String>();
+    	searchParams.add(state);
+    	trips = spider.search(siteName, siteUrl+searchParams.get(0) + prefix, searchParams);
+    	
+    	for (TripFormat trip : trips) {
+			System.out.println(trip);
+			System.out.println();
+		}
 		
 //		String [] countries = Locale.getISOCountries();
 //		for (String string : countries) {
